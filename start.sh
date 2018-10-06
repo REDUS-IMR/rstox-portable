902d9d7026250cdd2d5be481f0f55a9becc0746a
Ibrahim Umar <ibrahim.umar@uit.no>
2018-10-06 11:06:21 +0200

    Add info on how to use

--- start.sh
+++ start.sh
@@ -3,3 +3,9 @@
 docker pull redusimr/rstox-portable
 docker run --name rstox_go -p 8888:8888 -dit redusimr/rstox-portable
 
+echo "# Redus Portable is now active, open terminal using this address: http://localhost:8888/terminal"
+echo "# in your browser and File Manager (to upload and download Workspace files) using your browser at"
+echo "# http://localhost:8888/workspace"
+echo "#"
+echo "# Enjoy! -IU"
+
